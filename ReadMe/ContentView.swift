1aed70cf7d40a0416ae24b7c3c72a1412567e0b2
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2021-04-19 11:20:12 -0600

    Initial Commit

---
+++ ReadMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReadMe
+//
+//  Created by Adam Garrett-Harris on 4/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Adam Garrett-Harris on 4/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
